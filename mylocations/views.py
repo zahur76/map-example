95c7eccfb3f98232b5290988a2e0ade7644e6fbb
zahur <zahurmeerun@hotmail.com>
2022-04-05 12:42:24 +0400

    initial: setup for django and osm

--- mylocations/views.py
+++ mylocations/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+def map(request):
+    """A view to return the index page"""
+    return render(request, "mylocations/map.html")
