101c3d8134ba026f561837f935d019d09d3f791c
zahur <zahurmeerun@hotmail.com>
2022-04-05 13:00:03 +0400

    add: map and center on mauritius

--- mylocations/static/mylocations/js/mylocations.js
+++ mylocations/static/mylocations/js/mylocations.js
@@ -1,5 +1,4 @@
 const copy = "© <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors";
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const osm = L.tileLayer(url, { attribution: copy });
-const map = L.map("map", { layers: [osm] });
-map.fitWorld();
+const map = L.map("map", { layers: [osm], center: [-20.211, 57.670], zoom: 10 });
